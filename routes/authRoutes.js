5a7aaa051732fe444d5fd4cd012e08c470e03b5c
abhijitkale-Auxirem <abhijitkale@auxirem.com>
2026-06-29 11:26:45 +0530

    base f/b1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,14 +5,23 @@ const router =
     express.Router();
 
 const {
-    signupUser
+    signupUser,
+    getUserProfile
 } = require(
     "../controllers/authController"
 );
 
+const authMiddleware = require("../middleware/authMiddleware");
+
 router.post(
     "/signup",
     signupUser
+);
+
+router.get(
+    "/profile",
+    authMiddleware,
+    getUserProfile
 );
 
 module.exports =
